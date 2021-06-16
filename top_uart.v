af283e06a2539cb20769fcef0ef2e3e50d60d7a4
Lưu Văn Đức <50072440+luuvanduc1999@users.noreply.github.com>
2021-06-16 08:08:26 +0700

    Update top_uart.v

--- top_uart.v
+++ top_uart.v
@@ -13,9 +13,9 @@ module top_uart
    )
    (
     input wire clk, reset,
-    input wire rd_uart, wr_uart, rx,
+    input wire rd_uart, wr_uart, 
     input wire [7:0] w_data,
-    output wire tx_full, rx_empty, tx,
+    output wire tx_full, rx_empty, 
     output wire [7:0] r_data
    );
 
@@ -23,31 +23,30 @@ module top_uart
    wire tick, rx_done_tick, tx_done_tick;
    wire tx_empty, tx_fifo_not_empty;
    wire [7:0] tx_fifo_out, rx_data_out;
+   wire tx,rx;
 
    //body
    baudrate_generator  #(.M(DVSR), .N(DVSR_BIT)) baud_gen_unit
       (.clk(clk), .reset(reset),  .s_tick(tick));
 
    uart_rx #(.DBIT(DBIT), .SB_TICK(SB_TICK)) uart_rx_unit
-      (.clk(clk), .reset(reset), .rx(rx), .s_tick(tick),
+      (.clk(clk), .reset(reset), .rx(tx), .s_tick(tick),
        .rx_done_tick(rx_done_tick), .dout(rx_data_out));
 
    Fifo_buffer #(.W(DBIT), .N(FIFO_W)) fifo_rx_unit
       (.clk(clk), .reset(reset), .rd(rd_uart),
        .wr(rx_done_tick), .data_w(rx_data_out),
        .empty(rx_empty), .full(), .data_r(r_data));
+       
+       
 
    Fifo_buffer #(.W(DBIT), .N(FIFO_W)) fifo_tx_unit
       (.clk(clk), .reset(reset), .rd(tx_done_tick),
        .wr(wr_uart), .data_w(w_data), .empty(tx_empty),
        .full(tx_full), .data_r(tx_fifo_out));
-
    uart_tx #(.DBIT(DBIT), .SB_TICK(SB_TICK)) uart_tx_unit
       (.clk(clk), .reset(reset), .tx_start(tx_fifo_not_empty),
        .s_tick(tick), .din(tx_fifo_out),
        .tx_done_tick(tx_done_tick), .tx(tx));
-
    assign tx_fifo_not_empty = ~tx_empty;
-   assign tx = rx;
-
 endmodule
